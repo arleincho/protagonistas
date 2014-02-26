b2b799df7628f5dd1b962d34481060d0072b7309
Arley Wilches Marcelo <arley.wilches@gmail.com>
2014-02-26 11:00:34 -0500

    push

--- default/app/controllers/index_controller.php
+++ default/app/controllers/index_controller.php
@@ -6,35 +6,6 @@
  */
 class IndexController extends AppController{
 
-    protected function before_filter(){
-        if (Input::isAjax()) {
-          View::select('response', NULL);
-          View::response('json');
-        }
-    }
-
     public function index(){
-    }
-
-    public function save(){
-        View::response('json');
-        $cupones = Load::model('cupones')->find_first('document=' . Input::post('document'));
-        if ($cupones){
-            $cupones->update($_REQUEST);
-        }else{
-            $cupones = Load::model('cupones');
-            $cupones->create($_REQUEST);
-        }
-        $this->data = array('success' => true);
-    }
-
-    public function verify(){
-        $this->data = array('exists' => false);
-        if(Input::hasGet('document')){
-            $document = Input::get('document');
-            if (Load::model('cupones')->find("document='{$document}'")){
-                $this->data['exists'] = true;
-            }
-        }
     }
 }
